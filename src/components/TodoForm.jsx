3688b602490e0b80ac1d14f9bf80e62ecfab1c84
Usman Sheikh <usman03228270254@gmail.com>
2026-04-01 02:05:00 +0500

    Update TodoForm.jsx

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -100,7 +100,7 @@ const TodoForm = ({ openForm, setOpenForm, handleSubmit, editingTodo, getTodaySt
                                     type="submit"
                                     className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
                                 >
-                                    Add Task
+                                   {editingTodo ? "Save Changes" : "Add Todo"}
                                 </button>
                             </div>
                         </form>
